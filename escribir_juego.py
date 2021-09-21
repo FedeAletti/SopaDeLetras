c1b54b3c13a1cf0cf2caeec4a2f8d72120cf5e6e
Federico Aletti <fedeale2000@gmail.com>
2021-09-21 00:43:01 -0300

    Nombre archivos

--- escribir_juego.py
+++ escribir_juego.py
@@ -7,18 +7,31 @@ pedir_datos_tablero()
 generar_tablero()
 import csv
 celdas = []
-
+soluciones = []
+archivo_nom = pedirNombreArchivo()
 
 for i in range(N):
     colNum = str(i)
-    celdas.append(colNum)
+    celdas.append("Col" + colNum)
+
+for i in range(len(lista_palabras)):
+    palNum = str(i)
+    soluciones.append("Palabra" + palNum)
 
 
-with open(pedirNombreArchivo() + ".csv","w", newline='') as csvfile:
+with open(archivo_nom + ".csv","w", newline='') as csvfile:
     #fieldnames = celdas    
     writer = csv.writer(csvfile, delimiter="|")
 
     #writer.writeheader()
     writer.writerow(celdas)
     writer.writerows(tablero)
+
+with open(archivo_nom + "_solucion" + ".csv","w", newline='') as csvfile:
+    #fieldnames = celdas    
+    writer = csv.writer(csvfile, delimiter="|")
+
+    #writer.writeheader()
+    writer.writerow(soluciones)
+    writer.writerow(lista_palabras)
                       
